75a84c2b052b214522c77c28243d4b5bb2cd4088
TechiePotato <102460698+TechiePotato@users.noreply.github.com>
2022-08-12 12:32:57 +0530

    Update and rename client.js to Client.js

--- Structure/Client.js
+++ Structure/Client.js
@@ -42,6 +42,32 @@ class Erica extends Client {
 
 	}
 
+}const { Collection, MessageEmbed, Client, Intents } = require("discord.js");
+const mongoose = require("mongoose");
+
+class Erica extends Client {
+    	constructor(intents) {
+        super(intents);
+        
+        this.commands = new Collection;
+        this.slashCommands = new Collection;
+        this.config = require("../config");
+        this.prefix = this.config.Bot.BotPrefix;
+        mongoose.connect(this.config.Mongoose, {
+          useUnifiedTopology : true,
+          useNewUrlParser : true,
+          //useFindAndModify: false,
+        }).then(() => {
+          console.log(`Connected to Mongoose Data Base.`)
+        })
+    }
+    
+    start(token) {
+    if(!token) return console.error(`You forgot to provide the token of the bot.`)
+		this.login(token);
+	}
 }
+
+module.exports = Erica;
 
 module.exports = Erica;
